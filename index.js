a805b2e4d5aa07b0dbf4d36e9f6f317f599ca200
Megan Howley <meglhowley@gmail.com>
2021-05-10 14:23:41 -0400

    defined global variables and event listeners

--- index.js
+++ index.js
@@ -6,17 +6,34 @@ const twistItButton = document.querySelector('#twist-it')
 const flickItButton = document.querySelector('#flick-it')
 const pullItButton = document.querySelector('#pull-it')
 const spinItButton = document.querySelector('#spin-it')
-let gameChoices = ['bob-it!', 'flick-it!', 'pull-it!', 'twist-it!', 'spin-it!']
+const h1 = document.querySelector('h1')
+let gameChoices = ['bop-it!', 'flick-it!', 'pull-it!', 'twist-it!', 'spin-it!']
 let computerChoice = 'bop-it!'
+let playerChoice = ''
 
 // //////////////////////////////
 // Functions For Logic Here
 
+function computerChooses() {
+  const randomIndex = Math.floor(Math.random() * gameChoices.length)
+  computerChoice = gameChoices[randomIndex]
+  h1.innerText = computerChoice
+}
+
+setInterval(computerChooses, 2000)
+
+// function playerSelectsBopIt() {
+//   playerChoice = 'bop-it!'
+//   if (computerChoice === playerChoice) {
+//     computerChooses()
+//   }
+// }
+
 // // ////////////////////////////////
 // // // Event Listeners Here
 
-plusButton.addEventListener('click', addNumber)
-plusButton.addEventListener('click', addNumber)
-plusButton.addEventListener('click', addNumber)
-plusButton.addEventListener('click', addNumber)
-plusButton.addEventListener('click', addNumber)
+// bopItButton.addEventListener('click', playerSelectsBopIt)
+// twistItButton.addEventListener('click', playerSelectsTwistIt)
+// flickItButton.addEventListener('click', playerSelectsFlickIt)
+// pullItButton.addEventListener('click', playerSelectsPullIt)
+// spinItButton.addEventListener('click', playerSelectsSpinIt)
