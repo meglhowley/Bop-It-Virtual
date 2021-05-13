1074f69e82b3c2f3ca7ec7c4d256b9f3058c98f6
Megan Howley <meglhowley@gmail.com>
2021-05-13 14:17:27 -0400

    fixed double click audio issue

--- memory-mode/memory.js
+++ memory-mode/memory.js
@@ -9,20 +9,13 @@ const spinItButton = document.querySelector('#spin-it')
 const startButton = document.querySelector('#start-button')
 const replayButton = document.createElement('button')
 const h1 = document.querySelector('h1')
+h1.setAttribute('class', 'h1')
 const scoreBoard = document.querySelector('#score-board')
+const backToHomeDiv = document.querySelector('#back2h')
+const backToHomeLink = document.querySelector('#back2anchor')
 
 //Audio files
 const backgroundAudio = new Audio('../audio/background-song.mp3')
-const bopItSound = new Audio('../audio/bop-it-sound.mp3')
-const flickItSound = new Audio('../audio/flick-it-sound.mp3')
-const pullItSound = new Audio('../audio/pull-it-sound.mp3')
-const spinItSound = new Audio('../audio/spin-it-sound.mp3')
-const twistItSound = new Audio('../audio/twist-it-sound.mp3')
-const bopItCommand = new Audio('../audio/bop-it-command.mp3')
-const flickItCommand = new Audio('../audio/flick-it-command.mp3')
-const pullItCommand = new Audio('../audio/pull-it-command.mp3')
-const spinItCommand = new Audio('../audio/spin-it-command.mp3')
-const twistItCommand = new Audio('../audio/twist-it-command.mp3')
 const gameOverMain = new Audio('../audio/main-game-over-2.mp3')
 const backgroundSpeedUp1 = new Audio('../audio/background-speed-up-1.mp3')
 const transitionSound = new Audio('../audio/transition-sound.mp3')
@@ -37,7 +30,36 @@ let timeout = ''
 let highScore = 0
 
 const startGame = () => {
-  computerChooses()
+  setTimeout(computerChooses, 3000)
+  backgroundAudio.play()
+}
+
+const styleOpacity = () => {
+  h1.style.opacity = '0'
+}
+
+function playBopItCommand() {
+  let bopItSound1 = new Audio('../audio/bop-it-sound.mp3')
+  bopItSound1.play()
+}
+
+function playTwistItCommand() {
+  let twistItSound1 = new Audio('../audio/twist-it-sound.mp3')
+  twistItSound1.play()
+}
+
+function playSpinItCommand() {
+  let spinItSound1 = new Audio('../audio/spin-it-sound.mp3')
+  spinItSound1.play()
+}
+
+function playFlickItCommand() {
+  let flickItSound1 = new Audio('../audio/flick-it-sound.mp3')
+  flickItSound1.play()
+}
+function playPullItCommand() {
+  let pullItSound1 = new Audio('../audio/pull-it-sound.mp3')
+  pullItSound1.play()
 }
 
 function delayLoop(time) {
@@ -45,27 +67,33 @@ function delayLoop(time) {
     setTimeout(function () {
       if (cpuArr[i] === 'bop-it!') {
         h1.innerHTML = cpuArr[i]
-        bopItCommand.play()
+        playBopItCommand()
+        h1.style.opacity = '1'
       } else if (cpuArr[i] === 'flick-it!') {
         h1.innerHTML = cpuArr[i]
-        flickItCommand.play()
+        playFlickItCommand()
+        h1.style.opacity = '1'
       } else if (cpuArr[i] === 'pull-it!') {
         h1.innerHTML = cpuArr[i]
-        pullItCommand.play()
+        playPullItCommand()
+        h1.style.opacity = '1'
       } else if (cpuArr[i] === 'twist-it!') {
         h1.innerHTML = cpuArr[i]
-        twistItCommand.play()
+        playTwistItCommand()
+        h1.style.opacity = '1'
       } else if (cpuArr[i] === 'spin-it!') {
         h1.innerHTML = cpuArr[i]
-        spinItCommand.play()
+        playSpinItCommand()
+        h1.style.opacity = '1'
       }
     }, time * i)
+    h1.style.opacity = '0'
   }
 }
 
 function displayCommands() {
   cpuArr.forEach((command) => {})
-  delayLoop(2000)
+  delayLoop(1000)
 }
 
 const computerChooses = () => {
@@ -89,7 +117,7 @@ function compareArrays() {
   if (playerArr[playerArr.length - 1] === cpuArr[playerArr.length - 1]) {
     console.log(`cpu: ${cpuArr}`)
     console.log(`player: ${playerArr}`)
-    setTimeout(computerChooses, 2000)
+    setTimeout(computerChooses, 1000)
     console.log('next up!')
   } else {
     console.log('game over!')
@@ -99,9 +127,10 @@ function compareArrays() {
 //player selections below
 
 function playerSelectsBopIt() {
+  const bopItSound = new Audio('../audio/bop-it-sound.mp3')
+  bopItSound.play()
   playerChoice = 'bop-it!'
   playerArr.push(playerChoice)
-  bopItSound.play()
   if (playerArr.length < cpuArr.length) {
     compareChoice()
   } else if (playerArr.length >= cpuArr.length) {
@@ -110,9 +139,11 @@ function playerSelectsBopIt() {
 }
 
 function playerSelectsTwistIt() {
+  const twistItSound = new Audio('../audio/twist-it-sound.mp3')
+  twistItSound.play()
+  h1.innerHTML = ''
   playerChoice = 'twist-it!'
   playerArr.push(playerChoice)
-  twistItSound.play()
   if (playerArr.length < cpuArr.length) {
     compareChoice()
   } else if (playerArr.length >= cpuArr.length) {
@@ -121,9 +152,11 @@ function playerSelectsTwistIt() {
 }
 
 function playerSelectsFlickIt() {
+  const flickItSound = new Audio('../audio/flick-it-sound.mp3')
+  flickItSound.play()
+  h1.innerHTML = ''
   playerChoice = 'flick-it!'
   playerArr.push(playerChoice)
-  flickItSound.play()
   if (playerArr.length < cpuArr.length) {
     compareChoice()
   } else if (playerArr.length >= cpuArr.length) {
@@ -132,9 +165,11 @@ function playerSelectsFlickIt() {
 }
 
 function playerSelectsPullIt() {
+  const pullItSound = new Audio('../audio/pull-it-sound.mp3')
+  pullItSound.play()
+  h1.innerHTML = ''
   playerChoice = 'pull-it!'
   playerArr.push(playerChoice)
-  pullItSound.play()
   if (playerArr.length < cpuArr.length) {
     compareChoice()
   } else if (playerArr.length >= cpuArr.length) {
@@ -143,14 +178,21 @@ function playerSelectsPullIt() {
 }
 
 function playerSelectsSpinIt() {
+  const spinItSound = new Audio('../audio/spin-it-sound.mp3')
+  spinItSound.play()
+  h1.innerHTML = ''
   playerChoice = 'spin-it!'
   playerArr.push(playerChoice)
-  spinItSound.play()
   if (playerArr.length < cpuArr.length) {
     compareChoice()
   } else if (playerArr.length >= cpuArr.length) {
     compareArrays()
   }
+}
+
+function addGushers() {
+  const newImage = document.createElement('img')
+  newImage.src = 'https://i.imgur.com/cSallsF.png'
 }
 
 // // ////////////////////////////////
@@ -163,3 +205,4 @@ twistItButton.addEventListener('click', playerSelectsTwistIt)
 flickItButton.addEventListener('click', playerSelectsFlickIt)
 pullItButton.addEventListener('click', playerSelectsPullIt)
 spinItButton.addEventListener('click', playerSelectsSpinIt)
+backToHomeLink.addEventListener('mouseover', addGushers)
